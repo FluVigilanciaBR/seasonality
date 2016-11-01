1a58b050b22160ea48e66809a7602ddce244c587
Marcelo F C Gomes <marfcg@gmail.com>
2016-11-01 10:31:58 -0200

    Create requirements-mem.R

---
+++ methods/mem/requirements-mem.R
@@ -0,0 +1,8 @@
+# Install devtools
+install.packages("devtools")
+
+# Load devtools to install packages from github repos:
+library(devtools)
+
+# Install MEM package from Marcelo F C Gomes repo:
+install_github("marfcg/mem")
